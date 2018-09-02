8db8143a82a02f8df8ac25bd859d906e9da1ff25
aeobourn <aeobourn@cs.arizona.edu>
2018-09-01 21:19:12 -0700

    Update Drill02.java
    starter code for drill2

--- src/edu/uacs210fall2018/drill02/Drill02.java
+++ src/edu/uacs210fall2018/drill02/Drill02.java
@@ -1,10 +1,42 @@
 package edu.uacs210fall2018.drill02;
 
+
 public class Drill02 {
 
+    // takes a number of characters to print as a parameter
+    // returns a string with one or two asterisks in the middle
+    // surrounded by alligators
+    public static String zigzag(int n) {
 
-    // Return true if the input val is even.
-    static boolean isEven(int val) {
-        return false;
     }
+
+    // takes a string and a character as parameters and returns 
+    // the string with all copies of the character moved to the 
+    // end and capitalized
+    public static String moveToEnd(String s, char c) {
+
+    }
+
+    // takes two strings as parameters
+    // returns the index of where the second string starts in the
+    // first. If the second isn't in the first, returns -1
+    public static int indexOf(String s1, String s2) {
+
+    }
+    
+    // takes a string and two characters as parameters
+    // returns a string that is the same as the passed in string
+    // except that all occurrences of the first character are replaced
+    // with the second
+    public static String replaceAll(String s, char from, char to) {
+
+    }
+
+    // takes an integer as a parameter
+    // returns true if the digits are in sorted order ascending, 
+    // false otherwise
+    public static boolean digitsSorted(int x) {
+
+    }
+
 }
