ec0d588f23d56f844e97e0d45f2d780e241e6a5d
Michelle Strout <mstrout@cs.arizona.edu>
2018-09-08 11:32:17 -0700

    Drill02 starting code is now fully stubbed out.

--- src/edu/uacs210fall2018/drill02/Drill02.java
+++ src/edu/uacs210fall2018/drill02/Drill02.java
@@ -7,21 +7,21 @@ public class Drill02 {
     // returns a string with one or two asterisks in the middle
     // surrounded by alligators
     public static String zigzag(int n) {
-
+        return "";
     }
 
     // takes a string and a character as parameters and returns 
     // the string with all copies of the character moved to the 
     // end and capitalized
     public static String moveToEnd(String s, char c) {
-
+        return "";
     }
 
     // takes two strings as parameters
     // returns the index of where the second string starts in the
     // first. If the second isn't in the first, returns -1
     public static int indexOf(String s1, String s2) {
-
+        return 0;
     }
     
     // takes a string and two characters as parameters
@@ -29,14 +29,14 @@ public class Drill02 {
     // except that all occurrences of the first character are replaced
     // with the second
     public static String replaceAll(String s, char from, char to) {
-
+        return "";
     }
 
     // takes an integer as a parameter
     // returns true if the digits are in sorted order ascending, 
     // false otherwise
     public static boolean digitsSorted(int x) {
-
+        return false;
     }
 
 }
