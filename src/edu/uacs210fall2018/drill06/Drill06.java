d4a65be96d2cf88c8d7bcd2b401db928bf77addb
Michelle Strout <mstrout@cs.arizona.edu>
2018-10-14 16:42:46 -0700

    Drill06 problems.

---
+++ src/edu/uacs210fall2018/drill06/Drill06.java
@@ -0,0 +1,31 @@
+package edu.uacs210fall2018.drill06;
+
+public class Drill06 {
+
+    public static <K, V> V returnValue(HashNode<K, V> node) {
+        // TODO: implement using HashNode methods
+        return null;
+    }
+
+    public static <K, V> K returnKey(HashNode<K, V> node) {
+        // TODO: implement using HashNode methods
+        return null;
+    }
+
+    public static <K, V> V findLastVal(HashNode<K, V> first) {
+        // TODO: implement using HashNode methods
+        return null;
+    }
+
+    public static <K, V> HashNode<K, V> findNodeByIndex(HashNode<K, V> first,
+            int index) {
+        // TODO: implement using HashNode methods
+        return null;
+    }
+
+    public static <K, V> int countNodes(HashNode<K, V> first) {
+        // TODO: implement using HashNode methods
+        return 0;
+    }
+
+}
